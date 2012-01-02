18148647a815a0607dda5fe6e2120e37fbc4b7dc
ajafari <>
2012-01-02 08:56:23 +0000

    removed those deleted classes from TopTreeProducer

--- SpecialTRootObjects/src/LinkDef.h
+++ SpecialTRootObjects/src/LinkDef.h
@@ -4,24 +4,19 @@
 #include "../interface/TRootParticle.h"
 #include "../interface/TRootMCParticle.h"
 #include "../interface/TRootJet.h"
+#include "../interface/TRootJPTJet.h"
 #include "../interface/TRootCaloJet.h"
 #include "../interface/TRootGenJet.h"
 #include "../interface/TRootPFJet.h"
 #include "../interface/TRootMuon.h"
-#include "../interface/TRootCosmicMuon.h"
-#include "../interface/TRootTrack.h"
 #include "../interface/TRootElectron.h"
-#include "../interface/TRootSignalEvent.h"
 #include "../interface/TRootMET.h"
+#include "../interface/TRootCaloMET.h"
+#include "../interface/TRootPFMET.h"
 #include "../interface/TRootGenEvent.h"
-#include "../interface/TRootNPGenEvent.h"
 #include "../interface/TRootGenTop.h"
+#include "../interface/TRootNPGenEvent.h"
 #include "../interface/TRootSpinCorrGen.h"
-#include "../interface/TRootWCandidate.h"
-#include "../interface/TRootTopCandidate.h"
-#include "../interface/TRootCompositeCandidate.h"
-#include "../interface/TRootSemiLepEvent.h"
-#include "../interface/TRootXYZPoint.h"
 #include "../interface/TRootVertex.h"
 #include "../interface/TRootHLTInfo.h"
 #include "../interface/TRootTagProbeObject.h"
@@ -31,24 +26,19 @@
 #include "../interface/TRootParticle.h"
 #include "../interface/TRootMCParticle.h"
 #include "../interface/TRootJet.h"
+#include "../interface/TRootJPTJet.h"
 #include "../interface/TRootCaloJet.h"
 #include "../interface/TRootGenJet.h"
 #include "../interface/TRootPFJet.h"
 #include "../interface/TRootMuon.h"
-#include "../interface/TRootCosmicMuon.h"
-#include "../interface/TRootTrack.h"
 #include "../interface/TRootElectron.h"
-#include "../interface/TRootSignalEvent.h"
 #include "../interface/TRootMET.h"
+#include "../interface/TRootCaloMET.h"
+#include "../interface/TRootPFMET.h"
 #include "../interface/TRootGenEvent.h"
-#include "../interface/TRootNPGenEvent.h"
 #include "../interface/TRootGenTop.h"
+#include "../interface/TRootNPGenEvent.h"
 #include "../interface/TRootSpinCorrGen.h"
-#include "../interface/TRootWCandidate.h"
-#include "../interface/TRootTopCandidate.h"
-#include "../interface/TRootCompositeCandidate.h"
-#include "../interface/TRootSemiLepEvent.h"
-#include "../interface/TRootXYZPoint.h"
 #include "../interface/TRootVertex.h"
 #include "../interface/TRootHLTInfo.h"
 #include "../interface/TRootTagProbeObject.h"
@@ -64,26 +54,22 @@
 #pragma link C++ class TopTree::TRootParticle;
 #pragma link C++ class TopTree::TRootMCParticle;
 #pragma link C++ class TopTree::TRootJet;
+#pragma link C++ class TopTree::TRootJPTJet;
 #pragma link C++ class TopTree::TRootCaloJet;
 #pragma link C++ class TopTree::TRootGenJet;
 #pragma link C++ class TopTree::TRootPFJet;
 #pragma link C++ class TopTree::TRootMuon;
-#pragma link C++ class TopTree::TRootCosmicMuon;
-#pragma link C++ class TopTree::TRootTrack;
 #pragma link C++ class TopTree::TRootElectron;
-#pragma link C++ class TopTree::TRootSignalEvent;
 #pragma link C++ class TopTree::TRootMET;
+#pragma link C++ class TopTree::TRootCaloMET;
+#pragma link C++ class TopTree::TRootPFMET;
 #pragma link C++ class TopTree::TRootGenEvent;
-#pragma link C++ class TopTree::TRootNPGenEvent;
 #pragma link C++ class TopTree::TRootGenTop;
+#pragma link C++ class TopTree::TRootNPGenEvent;
 #pragma link C++ class TopTree::TRootSpinCorrGen;
-#pragma link C++ class TopTree::TRootWCandidate;
-#pragma link C++ class TopTree::TRootTopCandidate;
-#pragma link C++ class TopTree::TRootCompositeCandidate;
-#pragma link C++ class TopTree::TRootSemiLepEvent;
-#pragma link C++ class TopTree::TRootXYZPoint;
 #pragma link C++ class TopTree::TRootVertex;
 #pragma link C++ class TopTree::TRootHLTInfo;
 #pragma link C++ class TopTree::TRootTagProbeObject;
+#pragma link C++ struct TopTree::triggeredObject;
 
 #endif
