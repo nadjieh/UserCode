005857a4370e0d4b0d2fb59df4ba898acb628847
ajafari <>
2012-02-19 16:03:11 +0000

    t-channel single top event selection

---
+++ AnalysisClasses/EventSelection/macro/t-SingleTopSelection/run.sh
@@ -0,0 +1,2 @@
+#./SelectAndSave input work/samples/t-chan-Sync/1-TOPTREE.root input work/samples/t-chan-Sync/2-TOPTREE.root out Test XSec 1 Lumi 1 preSelEff 1 JES 1 isData no
+./SelectAndSave input work/samples/t-chan-Sync/SingleMu-PR-V1_Second_10.root out boolshit XSec 1 Lumi 1 preSelEff 1 JES 1 isData yes
